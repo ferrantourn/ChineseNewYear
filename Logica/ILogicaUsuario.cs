97ebcc0da6adbace36a8802d8d4fbbe257759a44
Amalfi Marini <fito@fito.com>
2013-03-02 22:34:10 -0300

    Remoting
    Remoting

---
+++ Logica/ILogicaUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Entidades;
+
+namespace Logica
+{
+    public interface ILogicaUsuario
+    {
+        Cliente getCliente(Cliente c);
+        Cliente getCliente(int ci);
+        Empleado getEmpleado(Empleado e);
+        void NuevoCliente(Cliente c);
+        void NuevoEmpleado(Empleado e);
+        Usuario getLoginUsuario(string userName, string pass);
+        List<Cliente> ListarAlumnosSinMovimientos(int NumeroDias);
+        List<Empleado> ListarEmpleados();
+        List<Cliente> ListarClientes();
+        /// <summary>
+        /// ACTUALIZA EL STATUS DE ACTIVO/NOACTIVO DE UN CLIENTE
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="SetActiveStatus"></param>
+        void ActualizarStatusCliente(Cliente c, bool setActiveStatus);
+
+
+        /// <summary>
+        /// ACTUALIZA LA INFORMACION DE UN CLIENTE
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        void ModificarCliente(Cliente c);
+
+        /// <summary>
+        /// ACTUALIZA LA INFORMACION DE UN EMPLEADO
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        void ModificarEmpleado(Empleado e);
+    }
+
+}
